03cd52790acfc9c303d442ead4bc77b4f822a827
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2025-03-10 23:23:41 +0100

    Hide secret

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,5 @@
 import displayHome from "./displayHome.js";
+import token from "./settings.js";
 
 class fetchFromAPI {
     static async fetch(path, container) {
@@ -7,7 +8,7 @@ class fetchFromAPI {
             method: "GET",
             headers: {
                 accept: "application/json",
-                Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTk2OTA2N2EzMjE4Y2U0M2M0OTE1ODYwZmI1YTY4MSIsIm5iZiI6MTczOTg4NDM3My4yMTIsInN1YiI6IjY3YjQ4NzU1OTFkN2U2NmM2NTZkZDFmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6Br1nDrZmToRWUlTfdv90vyrGd0XTKU4tOu8X23lkBY`,
+                Authorization: `Bearer ${token}`,
             },
         };
 
