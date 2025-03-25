1a1c2d619977d93074d76dcb26a67c5392200214
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2025-03-26 00:16:35 +0100

    cutting file

--- js/button.js
+++ js/button.js
@@ -1,4 +1,5 @@
 import { fetchTV, fetchTrending } from "./fetch.js";
+import { handleSearch } from "./search.js";
 
 const trendingByDay = document.querySelector("#day");
 const trendingByWeek = document.querySelector("#week");
@@ -27,4 +28,33 @@ tvPopular.addEventListener("click", () => {
     tvTopRated.classList.remove("active");
     fetchTV("popular");
     tvPopular.classList.add("active");
+});
+
+const searchInput = document.querySelector(".search-container div > input");
+const searchButton = document.querySelector(".search-container div > button");
+const yearInput = document.querySelector(".year-input");
+const ratingInput = document.querySelector(".rating-input");
+
+searchInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        handleSearch();
+    }
+});
+
+searchButton.addEventListener("click", handleSearch());
+searchInput.addEventListener("input", handleSearch);
+
+yearInput.addEventListener("change", () => {
+    filters.date.year = parseInt(yearInput.value);
+    if (searchInput.value.trim()) {
+        handleSearch();
+    }
+});
+
+ratingInput.addEventListener("change", () => {
+    filters.rating.value = parseFloat(ratingInput.value);
+    if (searchInput.value.trim()) {
+        handleSearch();
+    }
 });
